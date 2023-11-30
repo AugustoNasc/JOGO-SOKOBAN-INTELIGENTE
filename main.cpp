3b5d3b23b382dbf8eac287a8b5a4612ad1ee353d
AugustoNasc <augustonascimentodeoliveira30@gmail.com>
2023-11-29 21:25:14 -0300

    após conclusao de nivel, ir para escolha de niveis no grafo

--- main.cpp
+++ main.cpp
@@ -1,14 +1,6 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
-#include "bibliotecas/mapa.hpp"
-#include <string>
-#include <string.h>
-#include <iostream>
-#include "bibliotecas/movimentos.hpp"
-#include "bibliotecas/voltajogada.hpp"
-#include "bibliotecas/guarda_nivel.hpp"
-#include "bibliotecas/tela.hpp"
-#include "bibliotecas/grafo.hpp"
+#include "jogo.hpp"
 
 #define QTD_QUADRADOS 12 
 #define LARGURA 600
@@ -24,10 +16,6 @@ int main()
     sf::RenderWindow window(sf::VideoMode(LARGURA, LARGURA), "Gasparzinho");
     window.setKeyRepeatEnabled(false); // Desativa a repetição de teclas
     GameScreen currentScreen = TITLE;
-
-    sf::Music jogando, abertura;
-    sf::SoundBuffer conseguiuBuffer;
-    sf::Sound conseguiu;
 
     direcaoPersonagem sentido = FRENTE;
     int level=1;
@@ -59,125 +47,56 @@ int main()
     atualiza_mapas_acessados(mapas_acessados);
 
     int voltando=0;
+    int verticeAtualPersonagem=0;
+    
     window.setFramerateLimit(144);
     sf::Clock clock; //marcar tempo, para não clicar instantaneamente assim que se troca de tela
+
     
-    while (window.isOpen()) {
-        
+    if(ha_jogo_salvo()){
+        tl::jogo_salvo(window);
 
-        window.clear();
-        switch (currentScreen) {
-            case TITLE: {
-                tl::titulo(window, currentScreen, clock);
+    // Aguarda a entrada do usuário
+    while (true)
+    {
+        sf::Event continueEvent;
+        while (window.pollEvent(continueEvent))
+        {
+            if(continueEvent.type == sf::Event::Closed){
+                window.close();
+                return 0;
             }
-            
-            break;
-
-            case MANUAL1: {
-                tl::Manual(window, 1, currentScreen, clock);
-            }
-            break;
-
-            case MANUAL2:{
-                tl::Manual(window, 2, currentScreen, clock);
-            }
-            break;
-
-            case CREDITO:{
-                tl::Creditos(window, currentScreen, clock);
-            }
-            break;
-
-            case ESCOLHER_NIVEL:{
-
-                Graph g;
-
-                grafo_do_jogo(g);
-
-                desenhaGrafo_e_direcionaMapa(g, window, mapa, currentScreen, gX, gY, level, mapas_acessados);
-                
-            }
-            break;
-
-            case GAMEPLAY:{
-
-                sf::Texture backgroundTexture;
-                mapa_desenhando(sentido, mapa, imagens, fundo, level, window);
-
-                sf::Sprite background(backgroundTexture);
-                sf::Vector2i posicaoMouse = sf::Mouse::getPosition(window);
-                double p = window.getSize().x / 600.0;
-                sf::Vector2f scaledMousePosition = sf::Vector2f(posicaoMouse.x / p, posicaoMouse.y / p);
-                
-                sf::RectangleShape mouse(sf::Vector2f(15, 15));
-                mouse.setPosition(scaledMousePosition);
-
-                sf::RectangleShape botaoNivel;
-                botaoNivel.setPosition(200, 500);
-                botaoNivel.setSize(sf::Vector2f(200, 60));
-
-                tl::legenda_gameplay(window, currentScreen, clock);
-
-                // Verificar a colisão
-                sf::Time elapsed = clock.getElapsedTime();
-                if (botaoNivel.getGlobalBounds().intersects(mouse.getGlobalBounds())&& elapsed.asSeconds() >= 1.0f) {
-                    if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
-                        // Ação quando o botão esquerdo do mouse é liberado
-                        currentScreen = MANUAL2;
+            if (continueEvent.type == sf::Event::KeyPressed)
+            {
+                if (continueEvent.key.code == sf::Keyboard::N)
+                {
+                    // Apaga arquivo
+                    FILE *file = fopen("jogadas/niveis_salvos.txt", "w");
+                    std::cout<<"apagado"<<std::endl;
+                    if (file != NULL) {
+                        fclose(file);
                     }
-                    clock.restart();
+                    atualiza_mapas_acessados(mapas_acessados);
+                    while(window.isOpen())
+                        carrega_jogo(window, currentScreen, clock, level, gX, gY, mapas_acessados, mapa, &voltando, endereco, arquivo, sentido, imagens, fundo, verticeAtualPersonagem);
                 }
-                else{
-                    sf::Event event;
-                    while (window.pollEvent(event)) {
-                        if (event.type == sf::Event::Closed)
-                        window.close();
-
-                        if(event.type == sf::Event::KeyPressed){
-                        
-                            mv::atualiza_posicao_jogador(gX, gY, mapa);
-                            mv::comando_por_tecla(event, sentido, voltando, gX, gY, mapa, level, clock);
-
-                        }
-                    }
+                else if (continueEvent.key.code == sf::Keyboard::S)
+                {
+                    verticeAtualPersonagem=ultimo_nivel_desbloqueado()-1;
+                    while(window.isOpen())
+                        carrega_jogo(window, currentScreen, clock, level, gX, gY, mapas_acessados, mapa, &voltando, endereco, arquivo, sentido, imagens, fundo, verticeAtualPersonagem);
                 }
+                break;
             }
-            break;
-
-            case PARABENS:{
-               
-
-            }
-            break;
-
-            default:
-            break;
         }
-
-        window.display();
-
-        if(finalizou_mapa(mapa)){
-        
-            //currentScreen = PARABENS;
-            vtj::apagar_jogadas(level);
-            if(level!=13){
-                level++;
-            }
-            else{
-                level=1;
-            }
-                salvar_nivel(level);
-                sprintf(endereco, "mapastxt/mapa%d.txt", level);
-                arquivo = fopen(endereco, "rt");
-                fread(mapa.mapa, sizeof(char), 12*13, arquivo);
-                declarar_posicoes_de_encaixe(&mapa);
-            mv::atualiza_posicao_jogador(gX, gY, mapa);
-            sentido=FRENTE;
-    
+        if (!window.isOpen())
+            break;
     }
-
-        
-}
+    }
+    else{
+        while(window.isOpen())
+            carrega_jogo(window, currentScreen, clock, level, gX, gY, mapas_acessados, mapa, &voltando, endereco, arquivo, sentido, imagens, fundo, verticeAtualPersonagem);
+    }
 
 
     delete[] imagens;
